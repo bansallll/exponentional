e6989f068f7534f8c1f8dfaa7971900e5775c543
bansallll <141201732+bansallll@users.noreply.github.com>
2024-03-29 13:44:55 +0530

    Add files via upload

---
+++ list search.py
@@ -0,0 +1,11 @@
+#when list is given
+#a=[9,10,-2,65,13]
+#print(max(a))
+
+#whne list is given by user
+z=[]
+a=int(input("list:"))
+for i in range(a):
+    c =int(input("ENTER THE NUMBER LISt"))
+    z.append(c)
+print(max(z))
