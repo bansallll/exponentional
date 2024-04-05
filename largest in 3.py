b36e91cf5df559b6768a11d0ed59212b1c734829
bansallll <141201732+bansallll@users.noreply.github.com>
2024-04-05 14:19:53 +0530

    Add files via upload

---
+++ largest in 3.py
@@ -0,0 +1,11 @@
+a=int(input("enter 1st number"))
+b=int(input("enter 2nd number"))
+c=int(input("enter 3rd number"))
+
+if(a>b and a>c):
+  print(a)
+elif(b>c and b>a):
+  print(b)
+else:
+  print(c)
+   
