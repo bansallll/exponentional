a0b52b08987b8f701c16f01d5ee06cbc48f39906
bansallll <141201732+bansallll@users.noreply.github.com>
2024-05-26 12:30:24 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,23 @@
+import random
+
+def number():
+    k=random.randint(1,3)
+    while True:
+        userinput=int(input("guess number between 1 to 50:"))
+        if(userinput==k): 
+            print("Congratulations! You did it :)")
+            break
+        elif(userinput<k):
+            print("Number is bigger then your guess")   
+        else:
+            print("Number is smaller then you guess")
+
+
+while True:
+    number()
+    a=int(input("Press 1 for play again:"))
+    if(a==1):
+        print("Game start")
+    else:
+        print("Thanks for playing!")
+        break  
