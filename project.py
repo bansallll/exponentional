8c92d41487b105d4e817ba41d76a226e657e69b2
bansallll <141201732+bansallll@users.noreply.github.com>
2024-05-26 13:00:50 +0530

    Project.py

--- project.py
+++ project.py
@@ -1,16 +1,18 @@
 import random
 
 def number():
-    k=random.randint(1,3)
+    k=random.randint(1,50)
     while True:
         userinput=int(input("guess number between 1 to 50:"))
         if(userinput==k): 
             print("Congratulations! You did it :)")
             break
         elif(userinput<k):
-            print("Number is bigger then your guess")   
+            print("Number is bigger then your guess")  
         else:
-            print("Number is smaller then you guess")
+            print("Number is smaller then you guess") 
+        
+        
 
 
 while True:
